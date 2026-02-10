fc8cb469a7cfe2950b6b8c6cea2cf289e7bd5443
ellos0 <mcgibbonellott@gmail.com>
2026-02-09 17:01:32 -0800

    added quiting, and basic text writing functionality

--- bin/dart_text.dart
+++ bin/dart_text.dart
@@ -1,19 +1,48 @@
-//import 'package:lib/ui.dart';
+import 'dart:io';
 import 'package:dart_console/dart_console.dart';
 
-void main() async {
-     final console = Console();
+void cleanup(Console c) {
+     c.rawMode = false;
+     c.clearScreen();
+}
 
-     console.rawMode = true;
-     console.clearScreen();
-     console.resetCursorPosition();
-     console.write(
-             'Console size is ${console.windowWidth} cols and ${console.windowHeight} rows.'
-    );
+void safe_exit(Console c,int r) {
+     cleanup(c);
+     exit(r);
+}
+
+void cl_print(Console c,bool corner, String text) {
+     //command line print
+     c.cursorPosition = corner ? Coordinate(0,0) : Coordinate(c.windowHeight,0);
+     c.write(text);
+}
+
+void init_console(Console c) {
+     c.rawMode = true;
+     c.clearScreen();
+     c.resetCursorPosition();
+}
+
+void main() async {
+    final c = Console();
+
+    init_console(c);
+
+    cl_print(c, false, "testing");
     
-    console.resetCursorPosition();
-    console.readKey();
-    console.rawMode = false;
-    console.writeLine();
+    c.resetCursorPosition();
+
+    Key k;
+    while (true) {
+          k = c.readKey();
+          switch (k.char) {
+               case 'q':
+                  safe_exit(c,0);
+                  break;
+               default:
+                  c.write(k.char);
+                  break;
+      }
+    }    
 }
 
